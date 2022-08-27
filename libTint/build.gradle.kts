2b057cb2aa4d432d41a51a9f162d5b3de3ff6dff
chr56 <_>
2022-08-27 13:49:22 +0800

    [libTint] clean dependencies

--- libTint/build.gradle.kts
+++ libTint/build.gradle.kts
@@ -43,8 +43,6 @@ android {
 
 dependencies {
     api(project(":libColorRes"))
-    api(project(":libPref"))
-    api(project(":libUtil"))
     compileOnly("androidx.appcompat:appcompat:1.4.1")
     compileOnly("com.google.android.material:material:1.4.0")
 }
