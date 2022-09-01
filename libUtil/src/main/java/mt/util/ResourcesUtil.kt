4286f4ba3ce183e4da52d841781600ba6fd85de5
chr56 <_>
2022-09-01 17:33:51 +0800

    [ResourcesUtil] add Context.resolveAttr()

--- libUtil/src/main/java/mt/util/ResourcesUtil.kt
+++ libUtil/src/main/java/mt/util/ResourcesUtil.kt
@@ -5,6 +5,8 @@ package mt.util
 import android.content.Context
 import android.content.res.Configuration
 import android.os.Build
+import android.util.TypedValue
+import androidx.annotation.AttrRes
 
 fun Context.isNightMode(): Boolean =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
@@ -13,3 +15,13 @@ fun Context.isNightMode(): Boolean =
         val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         nightModeFlags == Configuration.UI_MODE_NIGHT_YES
     }
+
+fun Context.resolveAttr(@AttrRes attr: Int, @AttrRes fallbackAttr: Int): Int {
+    val value = TypedValue()
+    theme.resolveAttribute(attr, value, true)
+    return if (value.resourceId != 0) {
+        attr
+    } else {
+        fallbackAttr
+    }
+}
