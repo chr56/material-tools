4c7f321098096f3324ae9d60ebc79d131804ca9f
chr56 <_>
2022-10-31 09:31:51 +0800

    [TextColor] remove deprecated methods

--- libUtil/src/main/java/mt/util/color/TextColor.kt
+++ libUtil/src/main/java/mt/util/color/TextColor.kt
@@ -10,85 +10,54 @@ import androidx.annotation.ColorInt
 import androidx.core.content.ContextCompat
 import mt.color.R
 
-/**
- * @param inDark true if the dark version wanted
- */
-@Deprecated("legacy")
-@ColorInt
-fun getPrimaryTextColor(context: Context, inDark: Boolean): Int =
-    ContextCompat.getColor(
-        context,
-        if (inDark) R.color.primary_text_default_lightmode
-        else R.color.primary_text_default_darkmode
-    )
-
-/**
- * @param inDark true if the dark version wanted
- */
-@Deprecated("legacy")
-@ColorInt
-fun getSecondaryTextColor(context: Context, inDark: Boolean): Int =
-    ContextCompat.getColor(
-        context,
-        if (inDark) R.color.secondary_text_default_lightmode
-        else R.color.secondary_text_default_darkmode
-    )
-
-/**
- * @param inDark true if the dark version wanted
- */
-@Deprecated("legacy")
-@ColorInt
-fun getPrimaryDisabledTextColor(context: Context, inDark: Boolean): Int =
-    ContextCompat.getColor(
-        context,
-        if (inDark) R.color.primary_text_disabled_lightmode
-        else R.color.primary_text_disabled_darkmode
-    )
-
-/**
- * @param inDark true if the dark version wanted
- */
-@Deprecated("legacy")
-@ColorInt
-fun getSecondaryDisabledTextColor(context: Context, inDark: Boolean): Int =
-    ContextCompat.getColor(
-        context,
-        if (inDark) R.color.secondary_text_disabled_lightmode
-        else R.color.secondary_text_disabled_darkmode
-    )
 
 @ColorInt
 fun Context.primaryTextColor(darkmode: Boolean): Int =
-    getPrimaryTextColor(this, !darkmode)
+    ContextCompat.getColor(
+        this,
+        if (darkmode) R.color.primary_text_default_darkmode
+        else R.color.primary_text_default_lightmode
+    )
 
 @ColorInt
 fun Context.secondaryTextColor(darkmode: Boolean): Int =
-    getSecondaryTextColor(this, !darkmode)
+    ContextCompat.getColor(
+        this,
+        if (darkmode) R.color.secondary_text_default_darkmode
+        else R.color.secondary_text_default_lightmode
+    )
 
 @ColorInt
 fun Context.primaryDisabledTextColor(darkmode: Boolean): Int =
-    getPrimaryDisabledTextColor(this, !darkmode)
+    ContextCompat.getColor(
+        this,
+        if (darkmode) R.color.primary_text_disabled_darkmode
+        else R.color.primary_text_disabled_lightmode
+    )
 
 @ColorInt
 fun Context.secondaryDisabledTextColor(darkmode: Boolean): Int =
-    getSecondaryDisabledTextColor(this, !darkmode)
+    ContextCompat.getColor(
+        this,
+        if (darkmode) R.color.secondary_text_disabled_darkmode
+        else R.color.secondary_text_disabled_lightmode
+    )
 
 @ColorInt
 fun Context.primaryTextColor(@ColorInt bgColor: Int): Int =
-    getPrimaryTextColor(this, isColorLight(bgColor))
+    primaryTextColor(!isColorLight(bgColor))
 
 @ColorInt
 fun Context.secondaryTextColor(@ColorInt bgColor: Int): Int =
-    getSecondaryTextColor(this, isColorLight(bgColor))
+    secondaryTextColor(!isColorLight(bgColor))
 
 @ColorInt
 fun Context.primaryDisabledTextColor(@ColorInt bgColor: Int): Int =
-    getPrimaryDisabledTextColor(this, isColorLight(bgColor))
+    primaryDisabledTextColor(!isColorLight(bgColor))
 
 @ColorInt
 fun Context.secondaryDisabledTextColor(@ColorInt bgColor: Int): Int =
-    getSecondaryDisabledTextColor(this, isColorLight(bgColor))
+    secondaryDisabledTextColor(!isColorLight(bgColor))
 
 @ColorInt
 fun Context.primaryTextColor(): Int =
