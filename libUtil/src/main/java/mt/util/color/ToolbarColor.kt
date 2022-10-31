014b4f24fb1f5942927181b52690d6018a6c711e
chr56 <_>
2022-10-31 09:40:07 +0800

    [ToolbarColor] cleanup

--- libUtil/src/main/java/mt/util/color/ToolbarColor.kt
+++ libUtil/src/main/java/mt/util/color/ToolbarColor.kt
@@ -8,20 +8,38 @@ import androidx.annotation.ColorInt
 
 @CheckResult
 @ColorInt
-fun toolbarIconColor(context: Context, @ColorInt toolbarColor: Int): Int {
-    return if (isColorLight(toolbarColor)) {
-        toolbarSubtitleColor(context, toolbarColor)
+fun toolbarIconColor(context: Context, @ColorInt toolbarBackgroundColor: Int): Int {
+    return if (isColorLight(toolbarBackgroundColor)) {
+        toolbarSubtitleColor(context, toolbarBackgroundColor)
     } else {
-        toolbarTitleColor(context, toolbarColor)
+        toolbarTitleColor(context, toolbarBackgroundColor)
     }
 }
 
 @CheckResult
 @ColorInt
-fun toolbarSubtitleColor(context: Context, @ColorInt toolbarColor: Int) =
-    context.primaryTextColor(toolbarColor)
+fun toolbarSubtitleColor(context: Context, @ColorInt toolbarBackgroundColor: Int) =
+    context.primaryTextColor(toolbarBackgroundColor)
 
 @CheckResult
 @ColorInt
-fun toolbarTitleColor(context: Context, @ColorInt toolbarColor: Int) =
-    context.primaryTextColor(toolbarColor)
+fun toolbarTitleColor(context: Context, @ColorInt toolbarBackgroundColor: Int) =
+    context.primaryTextColor(toolbarBackgroundColor)
+
+@CheckResult
+@ColorInt
+@JvmName("toolbarIconColorKt")
+fun Context.toolbarIconColor(@ColorInt toolbarBackgroundColor: Int): Int =
+    toolbarIconColor(this, toolbarBackgroundColor)
+
+@CheckResult
+@ColorInt
+@JvmName("toolbarSubtitleColorKt")
+fun Context.toolbarSubtitleColor(@ColorInt toolbarBackgroundColor: Int): Int =
+    toolbarSubtitleColor(this, toolbarBackgroundColor)
+
+@CheckResult
+@ColorInt
+@JvmName("toolbarTitleColorKt")
+fun Context.toolbarTitleColor(@ColorInt toolbarBackgroundColor: Int): Int =
+    toolbarTitleColor(this, toolbarBackgroundColor)
