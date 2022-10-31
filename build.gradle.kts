9c373c370b829d41fd76d86a20281b0da09c0f74
chr56 <_>
2022-10-31 10:06:25 +0800

    [Gradle] bump the version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,4 @@ plugins {
 tasks.create(name = "clean", type = Delete::class) {
     doLast { delete(rootProject.buildDir) }
 }
-val libVersion: String by extra("0.0.8")
+val libVersion: String by extra("0.0.9")
